182a1ee087e8b0345ee256e557fc77a3e30614a8
hepao <50034742+hepao@users.noreply.github.com>
2019-05-29 20:40:47 -0500

    Lorem
    Lorem

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,4 +7,8 @@
     <li>Acerca de </li>
     <li>Login</li>
 </ul>
+
+
+
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur cumque delectus ex, expedita harum illum inventore ipsum modi nesciunt perspiciatis provident quas quasi, quis, ratione repellat repellendus repudiandae soluta tenetur!
      @endsection
