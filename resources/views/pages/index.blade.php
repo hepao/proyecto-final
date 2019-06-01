7509dcb646340cd1c876752ed20387f6c4d5525c
hepao <50034742+hepao@users.noreply.github.com>
2019-05-31 21:41:00 -0500

    proyecto final 29052019
    creacion proyecto andy

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,14 +1,45 @@
 @extends('layout.plantilla')
 
 @section('contenido')
-<ul>
-    <li>Inicio</li>
-    <li>Quines Somos</li>
-    <li>Acerca de </li>
-    <li>Login</li>
-</ul>
+ <section>
+     <div class="container">
+         <div class="row">
+             <div class="col-md-12">
+             <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                 <ol class="carousel-indicators">
+                     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                     <li data-target="#carouselExampleIndicators" data-slide-to="1" class="active"></li>
+                     <li data-target="#carouselExampleIndicators" data-slide-to="2" class="active"></li>
+                 </ol>
+                 <div class="carousel-inner">
+                     <div class="carousel-item active">
+                         <img src="css/img2.png" class="d-block w-100" alt="...">
+                     </div>
+                     <div class="carousel-item active">
+                         <img src="css/img2.png" class="d-block w-100" alt="...">
+                     </div>
+                     <div class="carousel-item active">
+                         <img src="css/img2.png" class="d-block w-100" alt="...">
+                     </div>
+                 </div>
+                 <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                     <span class="sr-only">Previous</span>
+                 </a>
+                 <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                     <span class="sr-only">Next</span>
+                 </a>
+             </div>
+
+             </div>
+         </div>
+
+     </div>
 
 
 
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur cumque delectus ex, expedita harum illum inventore ipsum modi nesciunt perspiciatis provident quas quasi, quis, ratione repellat repellendus repudiandae soluta tenetur!
+ </section>
+
+
      @endsection
